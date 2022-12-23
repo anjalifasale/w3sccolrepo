c2f91d6ce76d022a0306303ab0ebd93e0468914c
anjali <fasale.anjali123@gmail.com>
2022-12-22 22:37:32 -0800

    first commit

---
+++ p1.py
@@ -0,0 +1,168 @@
+# brand=input("enterthebrand")
+# if brand=='ro':
+#     print("it is too strong")
+#
+# elif brand=='so':
+#     print("medium")
+#
+# elif brand=='po':
+#     print("not that much kick")
+#
+# else:
+#     print("no otherrecommanded")
+
+# find biggest no from input
+# n1=int(input("enter first number"))
+# n2=int(input("enter secound number"))
+# n3=int(input("enter third number"))
+# if n1>n2 and n1>n3:
+#     print("n1 is greater")
+# elif n2>n1 and n2>n3:
+#     print("n2 is greater")
+# else:
+#     print("n3 is greater")
+
+# n1=input("enter first number")
+# n2=input("enter secound number")
+# n3=input("enter third number")
+# if n1>n2 and n1>n3:
+#     print("n1 is greater")
+# elif n2>n1 and n2>n3:
+#     print("n2 is greater")
+# else:
+#     print("n3 is greater")
+
+# no=int(input("enter no"))
+# if no%2==0:
+#     print("eeven")
+# else:
+#     print("odd")
+
+# no in 100 range or not
+# no=int(input("enter no"))
+# # if no>=1 and no<=100:
+# #     print("nube rin between 100")
+# # else:
+# #     print("not in 100")
+#
+# print(1<=no<=100)
+#
+# no=int(input("enter digit rom 0 to 9"))
+# if no==0:
+#     print("ZERO")
+#
+# elif no==1:
+#     print("ONE")
+# elif no == 2:
+#     print("TWO")
+# elif no == 3:
+#     print("THREE")
+# elif no==4:
+#     print("FOUR")
+# elif no==5:
+#     print("FIVE")
+# elif no==6:
+#     print("SIX")
+# else:
+#     print("no not in between 6")
+
+# iterative statements
+# if we want to reapeat group f statement repetadely
+# 1.for
+# 2.while
+
+# print each element present in list
+# l=[1,2,3,4,45,6]
+# for x in l:
+#     print(x)
+
+# # 1.print chrecterindex wise
+# s=input("enter string")
+# i=0
+# for x in s:
+#     if i!=0:
+#         print("charecter present at",i,"is",x)
+#     i+=1
+
+# no=int(input("enter no"))
+# fact=1
+# i=1
+# while i<=no:
+#     fact=fact*i
+#     i+=1
+# print("factorial is",fact)
+
+# for x in range(10):
+#     print(x)
+# for x in range(21):
+#     if x%2==0:
+#         print(x)
+
+# for x in range(1,21,2):
+#     print(x)
+# for x in range(10,1,-1):
+#     print(x)
+# l1=[10,20,30,40]
+# sum=0
+# for x in l1:
+#     sum+=x
+# print("sum",sum)
+# print n 1 10 in while loop
+
+# x=1
+# while x<=10:
+#     print(x)
+#     x+=1
+# sum fo no in while
+# n=int(input("enter no"))
+# sum=0
+# i=1
+# while i<=n:
+#     sum=sum+i
+#     i+=1
+# print(sum)
+
+
+# name=""
+# password=""
+# while name!='durga' and password!="python":
+#     name=input("enter name ")
+#     password=input("enter password")
+#
+# print("hello durga this is yur class")
+
+# for i in range(4):
+#     for j in range(4):
+#         print('i={} and j={}'.format(i,j))
+
+# *
+# **
+# ***
+# ****
+# *****
+#
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print("*",end="")
+#     print()
+
+# ******
+# *****
+# ****
+# **
+# *
+#
+# for i in range(6,1,-1):
+#     for j in range(i,1,-1):
+#         print("*",end="")
+#     print()
+
+# ***
+# ***
+# ***
+# for i in range(3):
+#     for j in range(3):
+#         print("*",end=" ")
+#     print()
+
+no=int(input("enter the "))
